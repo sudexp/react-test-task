a1c17bf8f23c30048a683aaddc10346c9b018f76
sudexp <sudexp@icloud.com>
2023-07-26 22:30:07 +0300

    getResult memoization

--- src/components/IsFive.jsx
+++ src/components/IsFive.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 let renderCount = 0;
 
 const IsFive = ({ value }) => {
   console.warn(`🔴 isFive render: ${++renderCount}`);
 
-  const getResult = () => {
+  const getResult = useMemo(() => {
     let i = 0;
 
     while (i < 500000000) i++; // simulate a heavy calculation
     return value === 5 ? '✅ It is five :D' : '❌ It is not five :(';
-  };
+  }, [value]);
 
-  return <h3>{getResult()}</h3>;
+  return <h3>{getResult}</h3>;
 };
 
 export default IsFive;
